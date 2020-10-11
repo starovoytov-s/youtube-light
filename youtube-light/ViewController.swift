a7d6bb3b1aadf0f9acd842b40936be6056b36e6b
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-10-11 21:26:56 +0300

    Initial Commit

---
+++ youtube-light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-light
+//
+//  Created by Stanislav Starovoytov on 11.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
